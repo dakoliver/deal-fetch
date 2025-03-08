0e140ef703d09a3898e718b442cd89a8401d9e46
dakoliver <djoliver55@icloud.com>
2025-03-07 22:24:12 -0600

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🔥 Welcome to Deal Fetch</h1>
+      <p>Find the best deals in real-time.</p>
+    </div>
+  );
+}

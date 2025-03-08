783dbf12d1a4365957d7dbca82447408b296842f
dakoliver <djoliver55@icloud.com>
2025-03-07 22:59:11 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  output: "export",
+  output: 'export',
 };
+
+module.exports = nextConfig;
